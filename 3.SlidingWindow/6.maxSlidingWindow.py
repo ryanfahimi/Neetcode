de23de2874bf0b47ad83297c2fd058745dd51966
Ryan Fahimi <ryanfahimi@gmail.com>
2024-03-17 23:25:14 -0700

    Refactor maxSlidingWindow method and add deque implementation

--- 3.SlidingWindow/6.maxSlidingWindow.py
+++ 3.SlidingWindow/6.maxSlidingWindow.py
@@ -1,4 +1,5 @@
 from typing import List
+from collections import deque
 
 # 239. Sliding Window Maximum
 # Hard
@@ -34,9 +35,9 @@ from typing import List
 # 1 <= k <= nums.length
 
 
-class Solution:
+class MaxSlidingWindow:
     # Time: O(k(n-k))
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+    def bruteForce(self, nums: List[int], k: int) -> List[int]:
         # Initialize the left and right pointers
         left, right = 0, k - 1
 
@@ -56,8 +57,8 @@ class Solution:
 
             # If the max index is within the window
             if maxIndex >= left:
-                # Update the max value and max index if nums[right] is larger
-                if nums[right] > maxValue:
+                # Update the max value and the max index
+                if nums[right] >= maxValue:
                     maxValue = nums[right]
                     maxIndex = right
             else:
@@ -70,17 +71,53 @@ class Solution:
 
         return result
 
+    def deque(self, nums: List[int], k: int) -> List[int]:
+        # Initialize the deque
+        dq = deque()
+
+        # Initialize the result array
+        result = []
+
+        # Initialize the left pointer
+        left = 0
+
+        # Slide the window over the array
+        for right, num in enumerate(nums):
+            # If the deque is not empty and the current element is greater than the last element in the deque
+            while dq and num > nums[dq[-1]]:
+                # Pop the last element from the deque
+                dq.pop()
+
+            # Append the current index to the deque
+            dq.append(right)
+
+            # If the index at the front of the deque is out of the window
+            if dq[0] < left:
+                # Pop the front element from the deque
+                dq.popleft()
+
+            # If the window size is equal to k
+            if (right + 1) >= k:
+                # Append the element at the front of the deque to the result array
+                result.append(nums[dq[0]])
+                # Increment the left pointer
+                left += 1
+
+        return result
+
     def main(self):
         nums = [1, 3, -1, -3, 5, 3, 6, 7]
         k = 3
         print(f"Input: nums = {nums}, k = {k}")
-        print(f"Output: {self.maxSlidingWindow(nums, k)}")
+        print(f"Brute Force Output: {self.bruteForce(nums, k)}")
+        print(f"Deque Output: {self.deque(nums, k)}")
 
         nums = [1]
         k = 1
         print(f"Input: nums = {nums}, k = {k}")
-        print(f"Output: {self.maxSlidingWindow(nums, k)}")
+        print(f"Brute Force Output: {self.bruteForce(nums, k)}")
+        print(f"Deque Output: {self.deque(nums, k)}")
 
 
 if __name__ == "__main__":
-    Solution().main()
+    MaxSlidingWindow().main()
